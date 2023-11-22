9751b2312a329eb20aff6dbee0907baedffbae08
Vinay Sayimpu <vinaysayimpu@gmail.com>
2023-11-22 18:13:49 +0530

    Minor styling changes in Welcome page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route, BrowserRouter as Router, Link } from 'react-router-dom';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import Signup from './Authentication/Signup';
 import Login from './Authentication/Login';
 import UserDashboard from './User_Dashboard/UserDashboard';
