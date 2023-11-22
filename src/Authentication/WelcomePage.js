11ac032d530fdc932ace9cf2b793da7a62b75418
Vinay Sayimpu <vinaysayimpu@gmail.com>
2023-11-22 18:08:49 +0530

    Minor changes in welcome page

--- src/Authentication/WelcomePage.js
+++ src/Authentication/WelcomePage.js
@@ -2,15 +2,16 @@
 import React from 'react';
 import MainLogo from '../Dorm-Logos/5.png';
 import { Link } from 'react-router-dom';
+import './WelcomePage.css';
 
 const WelcomePage = () => {
   return (
-    <div className='d-flex align-items-center justify-content-center vh-100 welcome-container'>
-      <div className='text-center w-50 vh-100 welcome-img-div align-items-center justify-content-center'>
+    <div className='d-flex align-items-center flex-sm-column flex-md-column flex-lg-row flex-xl-row justify-content-center vh-100 welcome-container'>
+      <div className='text-center w-50 w-sm-100 vh-100 welcome-img-div d-flex flex-column align-items-center justify-content-center img-div'>
         <img className="welcome-img" src={MainLogo} alt="DormEase Logo" />
         <p className='welcome-text'>Please select with the following --{'>'} </p>
       </div>
-      <div className='d-flex flex-column justify-content-center  w-50'>
+      <div className='d-flex flex-column justify-content-center w-50'>
         <div className="button-container">
           <Link to="/signup" className='entry-links'>
             <button className='entry-buttons'>Signup</button>
